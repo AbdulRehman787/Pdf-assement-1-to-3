0a01375d2e5246a3d4db02de4b24b733764bc8c8
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-21 08:18:07 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// show user warning on task 1
+// alert("Hello Mr Bean \n How r you ?.")
+
+
+// show user warning on task 2
+// alert("Hello World");
+// console.log("alert")
+
+
+// show user warning on task 3
+// var a = "Hello World";
+// alert(a);
+
+
+
+// show user warning on task 4
+// var b = "“A smarter way to learn JavaScript”.";
+// alert(b)
+
+
+
+
+// show user warning on task 5
+// var age = 18
+// alert(age)
+
+
+
+// show user warning on task 6
+// var birthYear = 2003;
+// document.write("<h1>Birth Year User</h1>")
+// document.write("30"+" " +"06"+ " "+ birthYear)
